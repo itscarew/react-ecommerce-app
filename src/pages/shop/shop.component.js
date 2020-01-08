ca1d37d9a2a61766cfd672b24b0796ba3a47f954
itscarew <itscarew@gmail.com>
2020-01-08 16:54:21 +0100

    updated app with shop page

---
+++ src/pages/shop/shop.component.js
@@ -0,0 +1,22 @@
+import React, { Component } from "react";
+import SHOP_DATA from "./shop.data";
+import CollectionPreview from "../../components/preview-collection/collection-preview.component";
+
+class ShopPage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { collections: SHOP_DATA };
+  }
+  render() {
+    const { collections } = this.state;
+    return (
+      <div className="shop-page">
+        {collections.map(collection => (
+          <CollectionPreview key={collection.id} {...collection} />
+        ))}
+      </div>
+    );
+  }
+}
+
+export default ShopPage;
