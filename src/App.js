bacef65b15a2a24c3663bca8c3f4acc3e54f8594
itscarew <itscarew@gmail.com>
2020-01-08 21:12:52 +0100

    sign in and sign up pages with input components

--- src/App.js
+++ src/App.js
@@ -3,13 +3,17 @@ import "./App.css";
 import HomePage from "./pages/homepage/homepage.component";
 import { Switch, Route } from "react-router-dom";
 import ShopPage from "./pages/shop/shop.component";
+import Header from "./components/header/header.component";
+import SignInAndSignUpPage from "./pages/sign-in-and-sign-out/sign-in-and-sign-out.component";
 
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/shop" component={ShopPage} />
+        <Route path="/signin" component={SignInAndSignUpPage} />
       </Switch>
     </div>
   );
---
+++ src/firebase/firebase.utils.js
@@ -0,0 +1,12 @@
+// const firebaseConfig = {
+//     apiKey: "AIzaSyDuEtYAhyufEJWrO2lYjr6SKRc4bmeNb-4",
+//     authDomain: "crown-ecommerce-a6fa6.firebaseapp.com",
+//     databaseURL: "https://crown-ecommerce-a6fa6.firebaseio.com",
+//     projectId: "crown-ecommerce-a6fa6",
+//     storageBucket: "crown-ecommerce-a6fa6.appspot.com",
+//     messagingSenderId: "986148196380",
+//     appId: "1:986148196380:web:cf76f780d401e57f4f0fd9",
+//     measurementId: "G-2KBJZ96K65"
+//   };
+//   // Initialize Firebase
+//   firebase.initializeApp(firebaseConfig);
