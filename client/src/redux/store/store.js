69e93a7c30ff22b434d34998c3758f89cade766e
itscarew <itscarew@gmail.com>
2020-09-04 18:06:18 +0100

    restructure

--- client/src/redux/store/store.js
+++ client/src/redux/store/store.js
@@ -11,8 +11,8 @@ const middlewares = [thunk];
 export const store = createStore(
   rootReducer,
   compose(
-    applyMiddleware(...middlewares),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    applyMiddleware(...middlewares)
+    // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
   )
 );
 
